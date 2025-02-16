855bfedafd024270d9db20e8aa7d3fa3616a0fce
nirat.ict <nirat.ict@gmail.com>
2025-02-16 12:10:25 +0700

    add woodenCrateCalculator

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -3,6 +3,7 @@ const { WebhookClient } = require("dialogflow-fulfillment");
 const { handleCalculator } = require("../handlers/intentHandlers");
 const { handleFallback } = require("../handlers/fallbackHandler");
 const { handleFeeCalculation } = require("../services/feeCalculator");
+const { handleWoodenCrateCalculation } = require("../services/woodenCrateCalculator");
 
 module.exports = (db) => {
   const router = express.Router();
@@ -16,6 +17,9 @@ module.exports = (db) => {
     );
     intentMap.set("Calculator", (agent) => handleCalculator(agent, db));
     intentMap.set("FeeCalculation", (agent) => handleFeeCalculation(agent, db));
+    intentMap.set("WoodenCrateCalculation", (agent) => 
+      handleWoodenCrateCalculation(agent, db)
+    );
 
     try {
       await agent.handleRequest(intentMap);
